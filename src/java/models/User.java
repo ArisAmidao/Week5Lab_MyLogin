f59b1f71b3c0d7b13216cfd37ecd602ce3e3e9d6
amida <amida@10.0.0.81>
2022-10-09 13:29:51 -0600

    Worked on AccountServices.java and User.java

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package models;
+import java.io.Serializable;
 
 /**
  *
  * @author amida
  */
-public class User {
+public class User implements Serializable{
+    private String username;
+    private String password;
     
+    
+    public User() {
+        this.username="";
+        this.password="";
+    }
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getUsername() {
+            return username;
+    }
+    
+    public void setUsername(String username){
+        this.username = username;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+            
 }
--- src/java/services/AccountServices.java
+++ src/java/services/AccountServices.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package services;
-
+import models.User;
 /**
  *
  * @author amida
  */
 public class AccountServices {
+    public AccountServices(){
+        
+    }
     
+    public User login(String username, String password) {
+        
+        if(username.equals("abe") || username.equals("barb") && password.equals("password")) {
+            User user = new User(username, null);
+            return user;
+        } else {
+            return null;
+        }
+    }
 }
